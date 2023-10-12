3e1aa389bf03209490c9bf138426844ef9c6a786
Ka. Santana <sdk.santana@gmail.com>
2023-10-12 18:28:56 -0300

    adicionando-funcionalidade-01

---
+++ CalculadorDeFrete/src/main/java/com/github/calculadordefrete/Caminhoes.java
@@ -0,0 +1,39 @@
+package com.github.calculadordefrete;
+
+public class Caminhoes {
+    String tipo;
+    double precoKM;
+    double capacidade;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getPrecoKM() {
+        return precoKM;
+    }
+
+    public void setPrecoKM(double precoKM) {
+        this.precoKM = precoKM;
+    }
+
+    public Caminhoes(String tipo, double precoKM, double capacidade) {
+        this.tipo = tipo;
+        this.precoKM = precoKM;
+        this.capacidade = capacidade;
+    }
+
+    public double getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(double capacidade) {
+        this.capacidade = capacidade;
+    }
+
+
+}
---
+++ CalculadorDeFrete/src/main/java/com/github/calculadordefrete/Produtos.java
@@ -0,0 +1,27 @@
+package com.github.calculadordefrete;
+
+public class Produtos {
+    String nome;
+    double peso;
+
+    public Produtos(String nome, double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+}
